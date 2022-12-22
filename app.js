3595e07690466ed299f445a452b495211bfc130c
Cemal SÖNMEZ <cemal.my@gmail.com>
2022-12-23 00:18:54 +0300

    handlebars ile layout yapısı kuruldu

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express')
+const {engine} = require('express-handlebars')
 const path = require("path")
 const app = express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views');
 
 app.use(express.static("public"))
 
 app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname, 'site/index.html'))
+  res.render("site/index")
 })
-
+app.get('/about', function (req, res) {
+  res.render("site/about")
+})
 app.get('/blog', function (req, res) {
-  res.sendFile(path.resolve(__dirname, 'site/blog.html'))
+  res.render("site/blog")
+})
+app.get('/contact', function (req, res) {
+  res.render("site/contact")
 })
 
-app.get('/test', function (req, res) {
-  res.send("TEST SAYFASI")
-})
 
 app.listen(3000)
