1bc067123f924b477dabdfff9fadaf72363cabfd
Cemal SÖNMEZ <cemal.my@gmail.com>
2022-12-25 23:53:26 +0300

    Login ve Register tasarımlar, Mongodbye giriş

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const {engine} = require('express-handlebars')
 const path = require("path")
 const app = express()
+
+mongoose.connect('mongodb://127.0.0.1:27017/nodejs')
+  .then(() => console.log('Connected!'));
 
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
@@ -20,6 +24,12 @@ app.get('/blog', function (req, res) {
 })
 app.get('/contact', function (req, res) {
   res.render("site/contact")
+})
+app.get('/login', function (req, res) {
+  res.render("site/login")
+})
+app.get('/register', function (req, res) {
+  res.render("site/register")
 })
 
 
