1bd0ed88fea7d4e72d9d50e4533ef478a6d59c90
Cemal SÖNMEZ <cemal.my@gmail.com>
2023-01-02 20:59:03 +0300

    Middleware, Helpers, Moment plugin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const fileUpload = require('express-fileupload')
+const generateDate = require('./helpers/generateDate').generateDate
 const {engine} = require('express-handlebars')
 const app = express()
 
@@ -10,9 +11,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/nodejs').then(() => console.log('Con
 app.use(fileUpload())
 app.use(express.static("public"))
 
-app.engine('handlebars', engine())
+
+app.engine('handlebars', engine({helpers:{generateDate:generateDate}}))
 app.set('view engine', 'handlebars')
 app.set('views', './views');
+
+const myMiddleware = (req, res, next) => {
+    console.log("Middleware")
+    next() // Middleware içindeki işlemler bitince sayfa açılışını sağlamak için kullanılır.
+}
+
+app.use("/", myMiddleware)
 
 const main = require("./routes/main")
 const posts = require("./routes/posts")
